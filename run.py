ec25abddc56ebdc07daec7e0c048cc7df3cee476
Andrey <leksus@leksus.info>
2021-03-16 19:35:09 +0300

    init

---
+++ run.py
@@ -0,0 +1,36 @@
+import argparse
+
+
+def get_cli_args():
+    """Parse args from cli"""
+    parser = argparse.ArgumentParser(
+        prog='streamit',
+        description='Stream videofiles to Youtube',
+    )
+    parser.add_argument(
+        'path',
+        type=str,
+        default='video',
+        help='folder with videofiles',
+    )
+    parser.add_argument(
+        '-k',
+        '--key',
+        type=str,
+        default='',
+        help='youtube stream key',
+    )
+    parser.add_argument(
+        '-u',
+        '--url',
+        type=str,
+        default='rtmp://a.rtmp.youtube.com/live2',
+        help='youtube stream url',
+    )
+
+    return parser.parse_args()
+
+
+def print_result_of(difference: str) -> None:
+    """Print difference of two files."""
+    print(difference)
